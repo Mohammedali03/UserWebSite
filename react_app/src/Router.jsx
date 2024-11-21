71f9c9529aef0d66c87d654cb1836b608a50b0ef
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2024-11-21 21:30:10 +0100

    second commit

--- react_app/src/Router.jsx
+++ react_app/src/Router.jsx
@@ -1,5 +1,27 @@
 import {createBrowserRouter  } from "react-router-dom";
-const router = createBrowserRouter([
+import Users from "./views/Users";
+import NotFound from "./views/NotFound";
 
+import Online from "./views/Online";
+import Offline from "./views/Offline";
+import Login from "./views/Login";
+
+const router = createBrowserRouter([
+{
+    path:'/online',
+    element: <Login/>
+},
+{
+    path:'/offline',
+    element: <Offline/>
+},
+{
+    path:'/Users',
+    element: <Users/>
+},
+{
+    path:'*',
+    element: <NotFound/>
+}
 ]) 
 export default router;
--- react_app/src/main.jsx
+++ react_app/src/main.jsx
@@ -1,12 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import router   from './Router.jsx';
 import './index.css'
-import App from './App.jsx'
-import Header from './Header.jsx'
+// import App from './App.jsx'
+// import Header from './Header.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
-    <App />
+    {/* <Header/>
+    <App/> */}
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
+
