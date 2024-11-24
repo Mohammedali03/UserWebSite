c0837c2beca0639473ae7efefe63fe82b29e722e
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2024-11-24 18:26:37 +0100

    storing data on the payload

--- react_app/src/views/SignUp.jsx
+++ react_app/src/views/SignUp.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom'
+import axiosClient from '../axios-client'
+import { UseStateContext } from '../contexts/ContextsProvider'
 
 export default function SignUp() {
+const nameRef                 = useRef()
+const emailRef                = useRef()
+const passwordRef                  = useRef()
+const passwordConfirmationRef = useRef()
+const {setUser,setToken} = UseStateContext()
+
   const onSubmit = (ev)=>{
     ev.preventDefault()
+    const payload = {
+      name              :   nameRef.current.value                , 
+      email            :    emailRef.current.value            ,
+      password        :     passwordRef.current.value               ,
+      password_confirmation: passwordConfirmationRef.current.value ,
+    }
+axiosClient.post("/SignUp",payload).then(({data})=>{
+  setUser(data.user)
+  setToken(data.token)
+}
+)
   }
   return (
     <div className="login-signup-form animated fadeInDown">
@@ -12,10 +31,10 @@ export default function SignUp() {
       <h1 className='title'>
      Create account
       </h1>
-<input type='text' placeholder='full name' />
-<input type='email' placeholder='Email' />
-<input type="password"  placeholder='Password'/>
-<input type="password"  placeholder='Password confirmation'/>
+<input ref={nameRef} type='text' placeholder='full name' />
+<input ref={emailRef} type='email' placeholder='Email' />
+<input ref={passwordRef} type="password"  placeholder='Password'/>
+<input ref={passwordConfirmationRef} type="password"  placeholder='Password confirmation'/>
 <button className="btn btn-block"> Signup</button>
 <p className='message'>
   Already registred? <Link to="/Login"> sign in to your account</Link>
