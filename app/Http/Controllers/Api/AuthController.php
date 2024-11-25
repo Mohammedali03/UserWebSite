f1896a2fde63414e72c8b6e5c783aafc504d1c6c
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2024-11-25 09:46:15 +0100

    authentification update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,22 +1,37 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
+use APP\Models\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login  (LoginRequest $request){
+   
+    public function signup (SignupRequest $request){
         $data =$request->validated();
  
-     $user=User::create([
-'name'=> $data['name'],
-'email'=>$data['email'],
-'password'=>bcrypt($data['password'])     ]);
+        $user=User::create([
+   'name'=> $data['name'],
+   'email'=>$data['email'],
+   'password'=>bcrypt($data['password'])     ]);
+   $token = $user->createToken('main')->plainTextToken;
+   return response(compact('user','token')) ;
     }
-    public function signup (SignupRequest $request){}
+    public function login  (LoginRequest $request){
+ $credentials = $request->validated();
+ if (!Auth::attempt($credentials)) {
+   return response([
+    'message'=>'provided email address or password is incorrect'
+   ]) ;
+ }
+ $user = Auth::user();
+ $token = $user->createToken('main')->plainTextToken;
+
+ return response(compact('user','token')) ;
+    }
     public function logout(Request $request){}
 }
